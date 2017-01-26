13cb3fedacc93724435e350213becc00ad344532
Harold Gomez <haroldgmz11@gmail.com>
2017-01-26 16:24:36 +0530

    I am screwed

--- Techword/urls.py
+++ Techword/urls.py
@@ -17,9 +17,16 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('zinnia.urls')),
 	url(r'^comments/', include('django_comments.urls')),
+
+
+
+    url(r'^about/$', TemplateView.as_view(template_name="checker.html")),
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ heehee/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HeeheeConfig(AppConfig):
+    name = 'heehee'
