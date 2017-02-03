c81f965d78146f0bc7c9ef08b87ccd82659e8030
Harold Gomez <haroldgmz11@gmail.com>
2017-02-04 00:38:37 +0530

    more chages on UI not cool but what I can.

--- Techword/settings.py
+++ Techword/settings.py
@@ -141,7 +141,6 @@ ZINNIA_UPLOAD_TO = 'media/uploads/zinnia'
 
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+from .secrets import EMAIL_HOST_USER,EMAIL_HOST_PASSWORD
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
