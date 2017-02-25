af933adac04dfe024269b3aa9230342d8484bf74
harold <haroldgmz11@gmail.com>
2017-02-25 22:58:36 +0530

    css fonts and more
    some changes

--- Techword/settings.py
+++ Techword/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6acev69+%z9if#1kz&kri9#%!#p=qwthp7*l^5l=bdk577a^u%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['testharold.com','192.168.1.2']
+ALLOWED_HOSTS = ['testharold.com','192.168.1.2','127.0.0.1']
 
 
 # Application definition
